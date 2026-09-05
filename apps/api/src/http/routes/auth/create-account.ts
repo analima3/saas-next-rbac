8d20911a42032af7e87a68101b3901bc48d9b8e2
analima3 <af.rl@hotmail.com>
2026-09-04 21:54:13 -0400

    feat: add swagger e vincula novo usuario a organizacao

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -4,18 +4,21 @@ import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
-const bodyCreateAccount = {
-  body: z.object({
-    name: z.string(),
-    email: z.email(),
-    password: z.string().min(6),
-  }),
-}
-
 export async function createAccount(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .post('/users', { schema: bodyCreateAccount }, async (request, reply) => {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        tags: ['User'],
+        summary: 'Create a new user account',
+        body: z.object({
+          name: z.string(),
+          email: z.email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
       const { name, email, password } = request.body
 
       const userWithSameEmail = await prisma.user.findUnique({
@@ -26,16 +29,33 @@ export async function createAccount(app: FastifyInstance) {
         return reply.status(400).send({ message: 'User already exists' })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
       const passwordHash = await bcrypt.hash(password, 6)
 
-      const user = await prisma.user.create({
+      await prisma.user.create({
         data: {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
 
       return reply.status(201).send({ message: 'User created successfully' })
-    })
+    }
+  )
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,5 +1,7 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import {
   jsonSchemaTransform,
   serializerCompiler,
@@ -13,8 +15,23 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(fastifyCors)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js saas',
+      description:
+        'Full-stack saas boilerplate built with Next.js, Fastify, Prisma, and Zod',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
 
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+app.register(fastifyCors)
 app.register(createAccount)
 
 app.listen({ port: 3333 }).then(() => {
