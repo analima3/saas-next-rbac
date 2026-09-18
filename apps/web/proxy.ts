a1feb8674d77307d14914b17ff76084d4092eeac
analima3 <af.rl@hotmail.com>
2026-09-18 16:23:45 -0400

    fix: redirect quando o usuario nao possui token

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,12 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 const publicRoutes = ['/auth/sign-in', '/auth/sign-up', '/auth/forgot-password']
+const protectedRoutes = ['/']
 
 export default async function proxy(request: NextRequest) {
   const token = request.cookies.get('token')?.value
+
   const { pathname } = request.nextUrl
 
   const isPublicRoute = publicRoutes.includes(pathname)
+  const isProtectedRoute = protectedRoutes.includes(pathname)
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL('/auth/sign-in', request.url))
+  }
 
   if (isPublicRoute && token) {
     return NextResponse.redirect(new URL('/', request.url))
@@ -16,5 +23,5 @@ export default async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/auth/:path*'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
